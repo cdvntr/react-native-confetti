90aba4102b0923a9828f90df05896fb1c7159f1e
Jack Thomson <Jack@mybuzztechnologies.co.uk>
2017-07-20 10:11:19 +0100

    Confetti falls within the container

--- confettiView.js
+++ confettiView.js
@@ -1,78 +1,104 @@
-import React, {Component} from 'react';
+import React, { Component } from "react";
 import {
-  AppRegistry,
-  StyleSheet,
-  View,
-  Animated
-} from 'react-native';
+	AppRegistry,
+	StyleSheet,
+	View,
+	Animated,
+	Dimensions
+} from "react-native";
 
 import Confetti from "./confetti.js";
 
+let windowHeight = Dimensions.get("window").height;
+let windowWidth = Dimensions.get("window").width;
+
 class ConfettiView extends Component {
-  constructor(props) {
-      super(props);
-      this.state = {confettis: []};
-      this.confettiIndex = 0;
-      this.shouldStop = false;
-  }
+	constructor(props) {
+		super(props);
+		this.state = { confettis: [], width: windowWidth, height: windowHeight };
+		this.confettiIndex = 0;
+		this.shouldStop = false;
+	}
 
-  startConfetti() {
-       let {confettis} = this.state;
-       let {confettiCount, timeout} = this.props;
-       this.shouldStop = false;
-       if(this.confettiIndex < confettiCount) {
-         setTimeout(() => {
-           if (this.shouldStop) {
-             return;
-           } else {
-             confettis.push({key: this.confettiIndex});
-             this.confettiIndex++;
-             this.setState({confettis});
-             this.startConfetti();
-           }
-         }, timeout);
-       }
-  }
+	startConfetti() {
+		let { confettis } = this.state;
+		let { confettiCount, timeout } = this.props;
+		this.shouldStop = false;
+		if (this.confettiIndex < confettiCount) {
+			setTimeout(() => {
+				if (this.shouldStop) {
+					return;
+				} else {
+					confettis.push({ key: this.confettiIndex });
+					this.confettiIndex++;
+					this.setState({ confettis });
+					this.startConfetti();
+				}
+			}, timeout);
+		}
+	}
 
-  removeConfetti(key) {
-       let {confettis} = this.state;
-       let {confettiCount} = this.props;
-       let index = confettis.findIndex(confetti => {return confetti.key === key});
-       confettis.splice(index, 1);
-       this.setState({confettis});
-       if(key === confettiCount - 1) {
-         this.confettiIndex = 0;
-       }
-  }
+	removeConfetti(key) {
+		let { confettis } = this.state;
+		let { confettiCount } = this.props;
+		let index = confettis.findIndex(confetti => {
+			return confetti.key === key;
+		});
+		confettis.splice(index, 1);
+		this.setState({ confettis });
+		if (key === confettiCount - 1) {
+			this.confettiIndex = 0;
+		}
+	}
 
-  stopConfetti ()
-  {
-    this.shouldStop = true;
-  }
+	onPositioned(event) {
+		let { x, y, width, height } = event.nativeEvent.layout;
+		this.setState({ width, height });
+	}
 
-  render() {
-       let {confettis} = this.state;
-       let {...otherProps} = this.props
-       return <View style={styles.container}>
-         {confettis.map(confetti => {
-             return <Confetti key={confetti.key} index={confetti.key} onComplete={this.removeConfetti.bind(this, confetti.key)} colors={this.props.colors} {...otherProps}/>
-         })}
-       </View>
-  }
+	stopConfetti() {
+		this.shouldStop = true;
+	}
+
+	render() {
+		let { confettis } = this.state;
+		let { ...otherProps } = this.props;
+		return (
+			<View
+				style={styles.container}
+				onLayout={event => this.onPositioned(event)}
+			>
+				{confettis.map(confetti => {
+					return (
+						<Confetti
+							key={confetti.key}
+							index={confetti.key}
+							containerWidth={this.state.width}
+							containerHeight={this.state.height}
+							onComplete={this.removeConfetti.bind(this, confetti.key)}
+							colors={this.props.colors}
+							{...otherProps}
+						/>
+					);
+				})}
+			</View>
+		);
+	}
 }
 
 ConfettiView.defaultProps = {
-   confettiCount: 100,
-   timeout: 30
-}
+	confettiCount: 100,
+	timeout: 30
+};
 
 const styles = StyleSheet.create({
-  container: {
-    position: 'absolute',
-    top: 0,
-    left: 0,
-    right: 0
-  }
+	container: {
+		position: "absolute",
+		top: 0,
+		left: 0,
+		right: 0,
+		bottom: 0
+	}
 });
 
 export default ConfettiView;
